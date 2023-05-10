1a4252a5b4d628a8782284e01433b1eae422c2ee
Nicklas <Nicklasrs@hotmail.com>
2023-05-10 22:17:29 +0200

    feat: add functions for TREC data subset and indexing
    This commit adds several functions to subset TREC-format files by selecting a specified number of documents and topics, write the subset to a new file, and create new index files using the subset. The `subset_data.py` is a new module with command-line functions that handle the subsetting of the TREC files. The `create_overlapping_subsets` function replaces `os.system` calls to subset the data and index the subset, making the commands more modular and Pythonic.

--- indexing/subset_and_index.py
+++ indexing/subset_and_index.py
@@ -1,8 +1,8 @@
 import os
 
 os.system(
-    "python indexing/subset_data.py create-overlapping-subsets --num-docs 10000 --num-topics 10000 --input-file data/trec/fulldocs-new.trec --output-file proc_data/train_trec/subset_msmarco.trec --topic-input-file train/queries.doctrain.tsv --topic-output-file proc_data/train_tsv/subset_queries.doctrain.tsv --qrels-input-file train/msmarco-doctrain-qrels.tsv --qrels-output-file proc_data/train_tsv/subset_msmarco-doctrain-qrels.tsv"
+    "python indexing/subset_data.py create-overlapping-subsets --num-docs 10000 --num-topics 10000"
 )
 
 # Run index_opt.py command
-os.system("python indexing/index.py proc_data/train_trec/ --output-folder index/")
+os.system("python indexing/index.py proc_data/train_trec/ --output-folder ./index/")
