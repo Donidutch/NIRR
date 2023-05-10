0bf1ddb295142e972fed6c70fe11c13e4f79630f
Nicklas <Nicklasrs@hotmail.com>
2023-05-10 18:22:02 +0200

    Add python script that subsets and indexes training data
    This commit adds a new file `subset_and_index.py` that subsets training data and creates an index to optimize queries. The script uses two commands: `subset-train-data` and `index_opt.py` to generate optimized files that are stored in the `./index/` directory.

---
+++ subset_and_index.py
@@ -0,0 +1,9 @@
+import os
+
+# Run subset-train-data command
+os.system(
+    "/NIR/.venv/bin/python /NIR/subset_data.py subset-train-data"
+)
+
+# Run index_opt.py command
+os.system("python index_opt.py proc_data/train_trec/ --output-folder ./index/")
